175323d8fcfcb1ab2a7a3bcffe5e28b3a5e0e03d
ArifTarp <arifoklh@gmail.com>
2021-01-15 13:00:15 +0300

    Changed default route map and Added home controller

--- Back-end/Asp.Net Core MVC Web User UI/Articles/Articles/Controllers/HomeController.cs
+++ Back-end/Asp.Net Core MVC Web User UI/Articles/Articles/Controllers/HomeController.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace Articles.Controllers
 {
-    public class PageController : Controller
+    public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            //TO DO api will be connect
-            return RedirectToAction("Index","Home");
+            return View();
         }
     }
 }
--- Back-end/Asp.Net Core MVC Web User UI/Articles/Articles/Startup.cs
+++ Back-end/Asp.Net Core MVC Web User UI/Articles/Articles/Startup.cs
@@ -53,7 +53,7 @@ namespace Articles
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Page}/{action=Index}/{id?}");
             });
         }
     }
